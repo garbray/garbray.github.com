095a0b72c02963409fd81ac22d20183029f74b06
Bryan Garzón Bahamón <darks@dksource.com.ar>
2012-07-07 13:08:19 -0500

    content dinamic added

---
+++ js/ext.js
@@ -0,0 +1,33 @@
+/************************************
+ * The Javascript prototype and 
+ * window extension
+ ************************************/
+
+Element.prototype.hasClassName = function(name) {
+  return new RegExp("(?:^|\\s+)" + name + "(?:\\s+|$)").test(this.className);
+};
+Element.prototype.addClassName = function(name) {
+  if (!this.hasClassName(name)) {
+    this.className = this.className ? [this.className, name].join(' ') : name;
+  }
+};
+Element.prototype.removeClassName = function(name) {
+  if (this.hasClassName(name)) {
+    var c = this.className;
+    this.className = c.replace(new RegExp("(?:^|\\s+)" + name + "(?:\\s+|$)", "g"), "");
+  }
+};
+
+window.getStyle = function(elem, styleProp){
+	return elem.currentStyle ? elem.currentStyle[styleProp] : window.getComputedStyle(elem,null).getPropertyValue(styleProp);
+};
+window.$ = function (){
+    return document.getElementById.apply(document, arguments)
+}
+window.$$ = function (){
+    return document.querySelectorAll.apply(document, arguments)
+}
+
+/************************************
+ * Event listener
+ ************************************/
--- js/script.js
+++ js/script.js
@@ -1,5 +1,74 @@
-/* Author:
+/************************************
+ author : Garbray
+ ************************************/
 
-*/
+/************************************
+ * Global variables
+ ************************************/
+
+var content = $('content'),
+	about = $('about'),
+	interesting = $('interesting'),
+	twitter = $('twitter'),
+	places = $('places'),
+	findme = $('findme'),
+	proyects = $('proyects')
+	cont = "";
 
 
+
+
+/************************************
+ * Event listener
+ ************************************/
+
+about.addEventListener('click', showAbout);
+findme.addEventListener('click',showFindMe);
+interesting.addEventListener('click',showInteresting);
+places.addEventListener('click',showPlaces);
+proyects.addEventListener('click',showProyects);
+twitter.addEventListener('click',showTwitter);
+
+
+/************************************
+ * Functions
+ ************************************/
+
+function showAbout(){
+	cont = "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>";
+	changeContent(cont);
+}
+
+function showFindMe(){
+	cont = "<h5>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h5>";
+	changeContent(cont);
+}
+
+function showPlaces(){
+	cont = "<p>Places</p>";
+	changeContent(cont);
+}
+
+function showProyects(){
+	cont = "<h2> fuck yeah </h2>";
+	changeContent(cont);
+}
+
+function showInteresting(){
+	cont = "<p> I love makes real my dreams this could be a really dangerous lol is only a test\
+				<ul> <li> like </li>\
+				</ul>\
+				</p>"
+	changeContent(cont);
+}
+
+function showTwitter(){
+	cont = "<p> Twitter</p>";
+	changeContent(cont);
+}
+
+
+
+function changeContent(text){
+	content.innerHTML = text;
+}
