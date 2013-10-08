894b6cea4e8fd50ddd6d627b14490e5e3dd8094f
garbray <darks@dksource.com.ar>
2013-10-07 20:27:04 -0500

    update grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,21 +4,34 @@ module.exports = function(grunt) {
         compass: {
             dist: {
                 options: {
-                    sassDir: 'sass',
-                    cssDir: 'css',
-                    raw: 'output-style = :compressed\n'
+                    config: 'config.rb',
+                    outputStyle: 'compressed'
                 }
             },
-        dev: {                    // Another target
-            options: {
-                config: 'config.rb',  // css_dir = 'dev/css'
+            dev: {                    // Another target
+                options: {
+                    config: 'config.rb'  // css_dir = 'dev/css'
+                }
             }
+        },
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                eqnull: true,
+                browser: true,
+                globals: {
+                    jQuery: true
+                },
+            },
+            src: ['js/modules/*.js']
         }
-      }
     });
 
     grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('build', ['compass:dev']);
+    grunt.registerTask('build', ['jshint','compass:dev']);
     grunt.registerTask('build_prod', ['compass:dist']);
 };
